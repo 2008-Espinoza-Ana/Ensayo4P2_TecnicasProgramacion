84484d6573e51463bca29c443996ccdec0fd973b
2008-Espinoza-Ana <espinozajimenez242008@gmail.com>
2026-07-21 21:33:54 -0500

    Add files via upload

---
+++ Ejercicio9Ensayo4.java
@@ -0,0 +1,24 @@
+package com.mycompany.main9;
+
+/**
+*
+* @author Espinoza Jiménez Ana María
+*/
+
+public class Main9 {
+
+    public static double calcularPromedio(double n1, double n2, double n3) {
+        return (n1 + n2 + n3) / 3;
+    }
+
+    public static void main(String[] args) {
+        double promedio = calcularPromedio(8.5, 7.0, 6.5);
+        System.out.println("El promedio es: " + promedio);
+
+        if (promedio >= 7) {
+            System.out.println("El estudiante aprobó.");
+        } else {
+            System.out.println("El estudiante reprobó.");
+        }
+    }
+}
